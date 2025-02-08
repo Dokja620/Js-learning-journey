a8130854b73214618720b8dfdb97be6a1c99ff38
Dokja <revanamike620@gmail.com>
2025-02-08 14:54:28 +0300

    Update person age and add approval check in index.js

--- basics/index.js
+++ basics/index.js
@@ -1,5 +1,16 @@
 let person = {
   firstName: "axel",
   lastName: "revana",
-  age: 20,
+  age: 2,
 };
+
+let isApproved;
+
+if (person.age > 20) {
+  isApproved = true;
+}
+
+if (isApproved) {
+  document.write("You are old enough to see this CORNNNNN!!!");
+}
+document.write("Hold on you are wayyy tooo young");
