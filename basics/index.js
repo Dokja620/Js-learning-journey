aa9ce674488d14fcb207b17e6551a5ef4b3d06f8
Dokja <revanamike620@gmail.com>
2025-02-08 15:05:13 +0300

    Update person age and implement age approval check in index.js

--- basics/index.js
+++ basics/index.js
@@ -1,23 +1,19 @@
 let person = {
   firstName: "axel",
   lastName: "revana",
-  age: 21,
+  age: 20,
 };
-let selectedColors = ["red", "blue", "green"];
-
-document.write("combien d'item dans couleur ? : ", selectedColors.length);
 
 let isApproved;
 
-if (person.age > 20) {
-  isApproved = true;
+function checkAge(age) {
+  if (age > 20) return (isApproved = true);
 }
 
+checkAge(person.age);
+
 if (isApproved) {
-  document.write("You are old enough to see this CORNNNNN!!!");
-  if (selectedColors[0] === "red") {
-    document.writeln("Good to know that you like ", selectedColors[0]);
-  }
+  document.write("Is approved");
 } else {
-  document.write("Hold on you are wayyy tooo young");
+  document.write("Is not approved");
 }
