9a603958efb29a513e14c8fee7d79747001dff72
Dokja <revanamike620@gmail.com>
2025-02-08 15:31:51 +0300

    Update index.html title and modify age check logic in index.js

--- basics/index.js
+++ basics/index.js
@@ -2,26 +2,40 @@ let person = [
   {
     firstName: "axel",
     lastName: "revana",
+    gender: "male",
     age: 20,
   },
   {
     firstName: "Diary",
     lastName: "revana",
+    gender: "female",
     age: 21,
+  },
+  {
+    firstName: "Mike",
+    lastName: "revana",
+    gender: "female",
+    age: 25,
   },
 ];
 
-let isApproved = false;
+let isApproved;
 
-function checkAge(age) {
-  if (age > 20) isApproved = true;
-  console.log("value is " + isApproved);
+function checkAge(age, fullName) {
+  if (age < 18) isApproved = false;
+
   if (!isApproved) {
-    document.write("Is not approved");
+    document.write(fullName + " You are old enough");
+    document.write(age + " is more than enough so CORNNNN");
   } else {
-    document.write("Is approved");
+    document.write("Access denied, Nice try tho" + fullName);
   }
 }
 
-checkAge(person[0].age);
-checkAge(person[1].age);
+function getFullName(firstName, lastName) {
+  let fullName = firstName + " " + lastName;
+
+  return fullName;
+}
+
+checkAge(person.age, getFullName(person.firstName, person.lastName));
