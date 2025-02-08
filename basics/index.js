457e0f28dbdf4313d165f675698a59a381c4e65b
Dokja <revanamike620@gmail.com>
2025-02-08 15:00:48 +0300

    Update person age and enhance color selection logic in index.js

--- basics/index.js
+++ basics/index.js
@@ -1,8 +1,11 @@
 let person = {
   firstName: "axel",
   lastName: "revana",
-  age: 2,
+  age: 21,
 };
+let selectedColors = ["red", "blue", "green"];
+
+document.write("combien d'item dans couleur ? : ", selectedColors.length);
 
 let isApproved;
 
@@ -12,5 +15,9 @@ if (person.age > 20) {
 
 if (isApproved) {
   document.write("You are old enough to see this CORNNNNN!!!");
+  if (selectedColors[0] === "red") {
+    document.writeln("Good to know that you like ", selectedColors[0]);
+  }
+} else {
+  document.write("Hold on you are wayyy tooo young");
 }
-document.write("Hold on you are wayyy tooo young");
